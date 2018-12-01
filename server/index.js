88752f8b5d044e45633b51a58c8bb0ea73de3d98
Bart Niessen <bart.niessen@gmail.com>
2018-12-01 10:23:34 +0100

    Shit wasn't saved...

--- server/index.js
+++ server/index.js
@@ -17,13 +17,12 @@ const { handleName } = require('./handleName.js');
 
 const rf = fs.readFileSync;
 // Set up
-console.log(constants.SSL_OP_NO_TLSv1);
 const app = express();
 const server = https.createServer({
   key: rf('../cert/SSLprivatekey.key'),
   cert: rf('../cert/SSLcertificate.crt'),
   ca: [rf('../cert/ca1.crt'), rf('../cert/ca2.crt')],
-  secureOptions: constants.SSL_OP_NO_TLSv1
+  secureOptions: constants.SSL_OP_NO_TLSv1_1
 }, app);
 const io = socket(server);
 
