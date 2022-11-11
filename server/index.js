fab20a15df86c334a6f26db7005c82690b5e0c09
Bart Niessen <bart.niessen@gmail.com>
2022-11-11 21:04:31 +0100

    Remove https, solved with reverse proxy. Fixes #90

--- server/index.js
+++ server/index.js
@@ -1,13 +1,10 @@
 // Simple server that receives a location via a REST API and has a connected socket!
 //@ts-check
 const http = require('http');
-const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
-const { constants } = require('crypto');
-
-const { currentTimeStamp } = require('./timestamp.js');
+const Logger = require('./logger');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -19,52 +16,39 @@ const config = require('../config/server.js');
 const PosCache = require('./PositionCache.js');
 const passwordHandler = require('./password');
 
-console.log(`[${currentTimeStamp()}][CORE] Node running on version: ${process.version}...`);
+const coreLog = new Logger('CORE');
+const appLog = new Logger('APP');
+const socketLog = new Logger('SOCKET');
+
+coreLog.log(`Node running on version: ${process.version}...`);
 
 const rf = fs.readFileSync;
 // Set up
 const app = express();
-const server = https.createServer({
-  key: rf(`${__dirname}/../cert/SSLprivatekey.key`),
-  cert: rf(`${__dirname}/../cert/SSLcertificate.crt`),
-  ca: [rf(`${__dirname}/../cert/ca1.crt`), rf(`${__dirname}/../cert/ca2.crt`)],
-  secureOptions: constants.SSL_OP_NO_TLSv1
-}, app);
+const server = http.createServer(app);
 const io = new Server(server);
 
-// The http server is just to redirect.
-const httpServer = http.createServer(app);
-
-// app.use(cors({ origin: /stamtour\.nl$/ }));
-
-// Redirect to https.
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Strict-Transport-Security", "max-age: 15552000; includeSubDomains");
-  if(!req.secure) {
-    console.log(`[${currentTimeStamp()}][HTTP] Insecure connection, redirect to https..: ${req.url} [full: https://${config.redirectUrl}/]`);
 
-    if (req.url.includes('.php')) {
-      return res.status(418).send('Konijnenboutje');
-    }
-
-    return res.redirect(`https://${config.redirectUrl}/`);
+  if (req.url.includes('.php')) {
+    return res.status(418).send('Konijnenboutje');
   }
   next();
 });
 
 // Simple request logger, just to see some activity.
 app.use((req, res, next) => {
-  console.log(`[${req.method}] Request to: ${req.path}, (ip: ${req.ip})`);
+  new Logger(req.method).log(`Request to: ${req.path}, (ip: ${req.ip})`);
   next();
 });
 
 app.use(bodyParser.json()); 
 
 server.listen(config.port);
-httpServer.listen(config.port + 1);
-console.log(`[${currentTimeStamp()}][CORE] Server listening on port: ${config.port}`);
+coreLog.log(`Server listening on port: ${config.port}`);
 
 ///////////////////////////////////////////////////////////////////////////////
 const positions = new PosCache();
@@ -86,12 +70,12 @@ const sendPosition = (data, errorFn) => {
   positions.addPosition(data);
 }
 const userLeft = (name = '__nameless__') => {
-  console.log(`[${currentTimeStamp()}][APP] User left: ${name}`);
+  appLog.log(`User left: ${name}`);
   positions.userOffline(name);
   broadcast('user-left', name);
 }
 const removeOfflineUser = (name) => {
-  console.log(`[${currentTimeStamp()}][APP] Removing offline user: ${name}`);
+  appLog.log(`Removing offline user: ${name}`);
   positions.removeUser(name);
   broadcast('user-destroyed', name);
 }
@@ -156,11 +140,11 @@ io
       }
     }
 
-    console.log('[SOCKET] A socket could not be connected.');
+    socketLog.log('A socket could not be connected.');
     next(new Error('Unable to authenticate.'));
   })
   .on('connection', (socket) => {
-    console.log(`[${currentTimeStamp()}][SOCKET] A socket connected ${socket.id}, using ${socket.conn.transport}`);
+    socketLog.log(`A socket connected ${socket.id}, using ${socket.conn.transport}`);
 
     const { token, requestPositions, access_token } = socket.handshake.query;
 
@@ -170,7 +154,7 @@ io
       const nameData = positions.registerUser(name, access_token, socket);
       name = nameData.name;
 
-      console.log(`[${currentTimeStamp()}][APP] User joined: ${name}`);
+      appLog.log(`User joined: ${name}`);
       broadcast('user-joined', name);
       process.nextTick(() => socket.emit('final-name', nameData));
     }
