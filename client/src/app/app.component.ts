9422661a65c18a8f488f52e705ddfdfa616dafa5
Bart <bart.niessen@fiqas.nl>
2024-01-13 22:16:31 +0100

    Het is mooi geweest  voor nu

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  protected readonly thisYear = new Date().getFullYear();
+
   ngOnInit () {
     setTimeout(() => window.scrollTo(0, 1), 10);
   }
